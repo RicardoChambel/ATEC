4f26b382ae7ed24f7720a0d0aad739e496c898f8
Ricardo Chambel <90462393+RicardoChambel@users.noreply.github.com>
2024-12-09 10:39:23 +0000

    Create dicionario.py

---
+++ PedroCardoso/dicionario.py
@@ -0,0 +1,11 @@
+# Cirar o dicionario key : valor
+carros = {
+    "Toyota" : "Carolla",
+    "Honda" : "Civic",
+    "Ford" : "Mustang",
+    "Tesla" : "Model 3",
+    "Toyota 2" : "Carolla" # Adicionar um elemento com a mesma key : valor || Não vai ser mostrado no print
+}
+
+# Mostrar
+print(carros)
