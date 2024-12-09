56f86c506306f6281214406c62bf37aa7f4ffd85
Ricardo Chambel <90462393+RicardoChambel@users.noreply.github.com>
2024-12-09 10:42:02 +0000

    Update validacoes.py

--- PedroCardoso/validacoes.py
+++ PedroCardoso/validacoes.py
@@ -31,19 +31,3 @@ def validar_marca():
             print("Erro: Entrada inválida. Digite apenas texto.")
 # Chamar função
 validar_marca()
-
-# VALIDAÇÃO DE EMAIL ------------------------------------------- 
-import re # --> Importar biblioteca de regex (regular expressions)
-# Função
-def validar_email():
-    while True:
-        email = input("Digite um email: ")
-        # Regex para validação básica de email
-        padrao = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        if re.match(padrao, email):
-            print(f"Email válido: {email}")
-            return email
-        else:
-            print("Erro: Email inválido. Tente novamente")
-# Chamar função
-validar_email()
